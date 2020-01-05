8864d05047bb7a33481a64b125eda4eac107d69d
Nikita <samartsevnikita@gmail.com>
2020-01-05 12:58:57 +0300

    Animation Views and Transitions.

---
+++ Landmarks/HikeDetail.swift
@@ -0,0 +1,49 @@
+//
+//  HikeDetail.swift
+//  Landmarks
+//
+//  Created by Nathan Algren on 03.01.2020.
+//  Copyright © 2020 Nikita Samartsev. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeDetail: View {
+    let hike: Hike
+    @State var dataToShow = \Hike.Observation.elevation
+    
+    var buttons = [
+        ("Elevation", \Hike.Observation.elevation),
+        ("Heart Rate", \Hike.Observation.heartRate),
+        ("Pace", \Hike.Observation.pace),
+    ]
+    
+    var body: some View {
+        VStack {
+            HikeGraph(hike: hike, path: dataToShow)
+                .frame(height: 200, alignment: .center)
+                //.offset(y: -10)
+            
+            HStack(spacing: 25) {
+                ForEach(buttons, id: \.0) { value in
+                    Button(action: {
+                        self.dataToShow = value.1
+                    }) {
+                        Text(verbatim: value.0)
+                            .font(.system(size: 15))
+                            .foregroundColor(value.1 == self.dataToShow
+                                ? Color.gray
+                                : Color.accentColor)
+                            .animation(nil)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HikeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeDetail(hike: hikeData[0])
+    }
+}
